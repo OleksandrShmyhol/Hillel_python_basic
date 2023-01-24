ac920d223612675fc86caf5dc0697ad4665ecbb4
Oleksandr Shmyhol <a.shmigol96@gmail.com>
2023-01-24 10:46:08 +0200

    home_work_2_and_3_of_lesson_2

--- Hillel_python_basic/lesson_2/home work_3_of_lesson_2.py
+++ Hillel_python_basic/lesson_2/home work_3_of_lesson_2.py
@@ -1,8 +1,5 @@
 user_number = int(input("Введіть 5-ти значне число:"))
-five_number = user_number % 10
-four_number = user_number // 10 % 10
-third_number = user_number // 100 % 10
-second_number = user_number // 1000 % 10
-first_number = user_number // 10000
-result = five_number * 10000 + four_number * 1000 + third_number * 100 + second_number * 10 + first_number * 1
-print(result)
+number = user_number % 10 * 10000 + user_number // 10 % 10 * 1000 + user_number // 100 % 10 * 100\
+         + user_number // 1000 % 10 * 10 + user_number // 10000 * 1
+print(number)
+
