60d143aa6b5220dc2539938ce8dda47d9baed8ee
Oleksandr Shmyhol <a.shmigol96@gmail.com>
2023-01-27 00:02:52 +0200

    home_work_5_of_lesson_3

---
+++ Hillel_python_basic/lesson_3/home_work_5_of_lesson_3.py
@@ -0,0 +1,22 @@
+# [1, 2, 3, 4, 5, 6] => [[1, 2, 3], [4, 5, 6]]
+# [1, 2, 3] => [[1, 2], [3]]
+# [1, 2, 3, 4, 5] => [[1, 2, 3], [4, 5]]
+# [1] => [[1], []]
+# [] => [[], []]
+
+lst = []
+first_list = []
+second_list = []
+if not len(lst):
+    print([first_list, second_list])
+elif not len(lst) % 2:
+    first_list = lst[:int(len(lst) / 2)]
+    second_list = lst[int(len(lst) / 2):]
+    print([first_list, second_list])
+elif len(lst) > 1:
+    first_list = lst[:int(len(lst) / 2) + 1]
+    second_list = lst[int(len(lst) / 2) * -1:]
+    print([first_list, second_list])
+else:
+    first_list += lst
+    print([first_list, second_list])
