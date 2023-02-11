7701486d6f048f841bece70dad7bc3fd1fd97ae6
Oleksandr Shmyhol <a.shmigol96@gmail.com>
2023-02-11 11:14:58 +0200

    home_work_17_of_lesson_7

---
+++ Hillel_python_basic/lesson_7/home_work_17_of_lesson_7.py
@@ -0,0 +1,11 @@
+def correct_sentence(word1):
+    word1 += '.' * (word1[-1] != '.')
+    return word1.capitalize()
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings, friends") == "Greetings, friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
