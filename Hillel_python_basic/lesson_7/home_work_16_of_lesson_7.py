62c86b86206d8085859d52819b94cb673fa4e84d
Oleksandr Shmyhol <a.shmigol96@gmail.com>
2023-02-11 10:46:48 +0200

    home_work_16_of_lesson_7

--- Hillel_python_basic/lesson_7/home_work_16_of_lesson_7.py
+++ Hillel_python_basic/lesson_7/home_work_16_of_lesson_7.py
@@ -4,3 +4,4 @@ def say_hi(name, age):
 
 assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
