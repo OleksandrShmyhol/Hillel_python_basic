9a8e2d9ca0d2650ea42a2a6a724653f4c23d63f3
Oleksandr Shmyhol <a.shmigol96@gmail.com>
2023-03-06 00:06:52 +0200

    home_work_29_of_lesson_11

---
+++ Hillel_python_basic/lesson_12/home_work_32_of_lesson_12.py
@@ -0,0 +1,64 @@
+class Item:
+
+    def __init__(self, name, price, description, dimensions):
+        self.price = price
+        self.description = description
+        self.dimensions = dimensions
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name}, price:{self.price}"
+
+
+class User:
+
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+class Purchase:
+    def __init__(self, user):
+        self.products = {}
+        self.user = user
+        self.total = 0
+
+    def add_item(self, item, cnt):
+        self.products[item] = cnt
+
+    def __str__(self):
+        tmp = ''
+        for item, cnt in self.products.items():
+            tmp += f"{str(item)}: {cnt} pcs. \n"
+        return f"User: {self.user.name} {self.user.surname}\n" \
+               f"Items:\n" \
+               f"{tmp}"
+
+    def get_total(self):
+        for item, cnt in self.products.items():
+            self.total += cnt * item.price
+        return self.total
+
+
+lemon = Item('lemon', 5, "yellow", "small", )
+apple = Item('apple', 2, "red", "middle", )
+print(lemon)  # lemon, price: 5
+
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)  # Ivan Ivanov
+
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 20 pcs.
+"""
+assert cart.get_total() == 60
